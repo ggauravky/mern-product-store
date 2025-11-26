651c3432260699fa1f5b4084bb3b9e7cb9cb333e
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-26 19:31:10 +0530

    Refactor server.js for improved clarity and consistency
    Reordered imports, renamed ProductRouter to productRoutes, and standardized PORT usage. Fixed static file serving in production by correcting path resolution. These changes improve code readability and maintainability.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
 import path from "path";
 
-import ProductRouter from "./routes/product.route.js";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-process.env.PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", ProductRouter);
+app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(pathspec.join(__dirname, "frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-app.listen(process.env.PORT , () => {
-  connectDB();
-  console.log("server started at http://localhost:" + process.env.PORT);
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
